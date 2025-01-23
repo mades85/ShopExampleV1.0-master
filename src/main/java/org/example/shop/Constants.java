a93b50363326f3e5f2599f52b020a9f9798bb6a8
Matthias <matthias-wenning@t-online.de>
2025-01-23 11:53:18 +0100

    added constants

---
+++ src/main/java/org/example/shop/Constants.java
@@ -0,0 +1,7 @@
+package org.example.shop;
+
+public interface Constants {
+    String DATA_SOURCE = "src/main/resources/data/electronics.csv";
+    String MESSAGE = "message";
+    int PAGE_SIZE = 15;
+}
